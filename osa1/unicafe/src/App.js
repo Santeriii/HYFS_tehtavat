6850b820bbe3c08ec42fc0beca18ae62b45b2822
Santeri Einola <62256499+Santeriii@users.noreply.github.com>
2021-03-30 21:18:34 +0300

    tehtävä 1.6 valmis

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleAddGood = () => {
+    setGood(good + 1)
+  }
+
+  const handleAddNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleAddBad = () => {
+    setBad(bad + 1)
+  }
+
+  const Button = ({ handleClick, text }) => {
+    return (
+      <button onClick={handleClick}>
+        {text}
+      </button>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Give feedback</h1>
+      <Button handleClick={handleAddGood} text={"good"} />
+      <Button handleClick={handleAddNeutral} text={"neutral"} />
+      <Button handleClick={handleAddBad} text={"bad"} />
+      <h1>statistics</h1>
+      good {good}<br/>
+      neutral {neutral}<br/>
+      bad {bad}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
