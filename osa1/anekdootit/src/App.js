ba9ddfe830a056028a764a1950187e45e8cf2913
Santeri Einola <einola.santeri@gmail.com>
2021-04-01 09:52:50 +0300

    tehtävä 1.12 valmis

---
+++ osa1/anekdootit/src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+
+const RandomAnecdote = ({ anecdotes, selected, setSelected }) => {
+  setSelected(Math.floor(Math.random() * 6))
+
+  const changeAnecdote = () => {
+    setSelected(Math.floor(Math.random() * 6))
+  }
+
+  return (
+    <div>
+      {anecdotes[selected]}<br />
+      <button onClick={changeAnecdote}>next anecdote</button>
+    </div>
+  )
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  ]
+  const [selected, setSelected] = useState(0)
+
+  return (
+    <RandomAnecdote anecdotes={anecdotes} selected={selected} setSelected={setSelected} />
+  )
+}
+
+export default App
