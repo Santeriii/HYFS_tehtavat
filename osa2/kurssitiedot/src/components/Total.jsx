5a52996a5861f70888e8ebc6e3872fec3c3923fa
Santeri Einola <einola.santeri@gmail.com>
2021-04-07 13:45:32 +0300

    tehtävä 2.3 valmis

--- osa2/kurssitiedot/src/components/Total.jsx
+++ osa2/kurssitiedot/src/components/Total.jsx
@@ -2,12 +2,16 @@ import React, { useEffect, useState } from 'react'
 
 const Total = parts => {
     const [total, setTotal] = useState(0)
+    let exercises = []
     const partArray = parts.parts
 
     useEffect(() => {
         partArray.map(part => {
-            setTotal(prevState => prevState + part.exercises)
+            const copy = exercises.concat(part.exercises)
+            exercises = copy
         })
+        
+        setTotal(exercises.reduce( (s, p) => s + p))
     }, [])
 
     return (
